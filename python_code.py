ff188ab0cca55a20415fcd47ebe731d775225cf5
anupabenny <anupabenny90@gmail.com>
2024-03-08 20:35:58 +0530

    Python code for demonstrating Multiprocessing

---
+++ python_code.py
@@ -0,0 +1,34 @@
+import multiprocessing
+
+def bds_worker(chunk, bds_result_queue):
+    bds_parallel_sum = sum(chunk)
+    bds_result_queue.put(bds_parallel_sum)
+
+def main():
+    bds_large_array = list(range(1, 100001))
+    nprocs = multiprocessing.cpu_count()
+    bds_chunk_size = len(bds_large_array) // nprocs
+    bds_chunks = [bds_large_array[i:i + bds_chunk_size] for i in range(0, len(bds_large_array), bds_chunk_size)]
+
+    bds_result_queue = multiprocessing.Queue()
+
+    bds_processes = []
+
+    for i in range(nprocs):
+        process = multiprocessing.Process(target=bds_worker, args=(bds_chunks[i], bds_result_queue))
+        bds_processes.append(process)
+        process.start()
+
+    for process in bds_processes:
+        process.join()
+
+    bds_results = []
+    while not bds_result_queue.empty():
+        bds_results.append(bds_result_queue.get())
+
+    bds_total_sum = sum(bds_results)
+
+    print("Total sum:", bds_total_sum)
+
+if __name__ == "__main__":
+    main()
